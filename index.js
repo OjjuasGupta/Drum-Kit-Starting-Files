8a0d8cfbba238c9255cdfbe815de03fc3368401b
OjjuasGupta <ojjuas4453@gmail.com>
2023-01-23 22:13:44 +0530

    comment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
+//creating a loop for all the buttons as their may be more buttons so .drum class specifies the buttons performing the following action
+//through the function
 
-for(var i=1;i<document.querySelectorAll(".drum").length;i++)
+for(var i=1;i<document.querySelectorAll(".drum").length;i++)        //till the length of the array
 
-document.querySelectorAll("button")[i].addEventListener("click",function()
+document.querySelectorAll("button")[i].addEventListener("click",function()      //here function is anonyomus no name provided
+                                                                                //[i] has been taken to click on all the keys
 {
     alert("I got clicked");
 }); 
