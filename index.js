f7cf96385170dbe04a642083b5591b297ff7289e
OjjuasGupta <ojjuas4453@gmail.com>
2023-01-24 00:58:21 +0530

    final

--- index.js
+++ index.js
@@ -61,4 +61,7 @@ switch (key) {
 function buttonAnimation(currentKey){
     var activeButton =document.querySelector("."+currentKey);
     activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
 }
