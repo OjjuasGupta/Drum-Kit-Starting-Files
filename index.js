34801954132fc7f9ad5f29218aa322a60464a603
OjjuasGupta <ojjuas4453@gmail.com>
2023-01-23 22:09:48 +0530

    new1

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-document.querySelector("button").addEventListener("click",function()
+
+
+for(var i=1;i<document.querySelectorAll(".drum").length;i++)
+
+document.querySelectorAll("button")[i].addEventListener("click",function()
 {
     alert("I got clicked");
 }); 
